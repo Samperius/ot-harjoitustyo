83611566afccf7f9a25b9b0dbbb30db11b225265
Samperius <samuli.kinnunen@gmail.com>
2023-03-27 12:12:28 +0300

    maksukortti

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,26 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.kortti = Maksukortti(1000)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.50 euroa")
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6.00 euroa")
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
+
