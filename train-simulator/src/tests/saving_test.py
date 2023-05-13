cf764ce0fb656198da3a4df1a07dbeea2d665c20
Samperius <samuli.kinnunen@gmail.com>
2023-05-13 12:06:10 +0300

    viikko 7

--- train-simulator/src/tests/saving_test.py
+++ train-simulator/src/tests/saving_test.py
@@ -10,24 +10,33 @@ RESULTS_PATH = os.path.join(dirname, "..", "data", RESULTS_FILENAME)
 
 class TestTrackRepo(unittest.TestCase):
     def setUp(self):
-        data = {'number_of_simulations': [1],
+        self.data = {'number_of_simulations': [1],
                 'number_of_trains': [4],
                 'average_waiting_time': [5.6]}
         self.saving = Saving(RESULTS_PATH)
-        self.saving.save_dataframe(data)
+        self.saving.save_dataframe(self.data)
+        self.df = pd.read_csv(RESULTS_PATH)
+        os.remove(RESULTS_PATH)
+
 
     def test_saving_simulations(self):
-        df = pd.read_csv(RESULTS_PATH)
-        simulations = df.iloc[-1,0]
+        simulations = self.df.iloc[-1,0]
         self.assertEqual(simulations, 1)
 
     def test_trains_trains(self):
-        df = pd.read_csv(RESULTS_PATH)
-        trains = df.iloc[-1,1]
+        trains = self.df.iloc[-1,1]
         self.assertEqual(trains, 4)
 
     def test_saving_waiting(self):
-        df = pd.read_csv(RESULTS_PATH)
-        waiting = df.iloc[-1,2]
+        waiting = self.df.iloc[-1,2]
         self.assertEqual(waiting, 5.6)
+
+    def test_file_does_not_exist(self):
+        self.saving.save_dataframe(self.data)
+        self.df = pd.read_csv(RESULTS_PATH)
+        waiting = self.df.iloc[-1, 2]
+        self.assertEqual(waiting, 5.6)
+        os.remove(RESULTS_PATH)
+
+
 
